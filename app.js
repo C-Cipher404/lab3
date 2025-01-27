065a798930f07e57a67b7cfd1ad070780bd6a041
Cortney Anderson <cipher404@Cortneys-MacBook-Pro.local>
2025-01-27 07:49:24 -0700

    listening on wrong port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   fs.createReadStream("data.txt").pipe(res);
 });
 
-server.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+server.listen(8080, () => {
+  console.log("Server is listening on port 8080");
 });
